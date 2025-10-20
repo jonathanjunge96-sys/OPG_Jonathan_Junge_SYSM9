aa72be8549a3d28fd93931e83310bca24c204678
Jonathan Junge <jonathan.junge96@gmail.com>
2025-10-20 23:50:04 +0200

    Startat

---
+++ CookMaster/Managers/UserManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using CookMaster.Models;
+
+namespace CookMaster.Managers
+{
+
+    public class UserManager : INotifyPropertyChanged 
+    {
+        private User _currentUser;
+        private readonly List<User> _users = new();
+        public UserManager() { }
+
+        public User CurrentUser
+        {
+            get => _currentUser;
+            private set
+            {
+                _currentUser = value;
+                OnPropertyChanged(nameof(CurrentUser));
+                OnPropertyChanged(nameof(IsAuthenticated));
+            }
+        }
+
+        public bool IsAuthenticated => CurrentUser != null;
+
+        public bool Login(string username, string password)
+        {
+            var user = _users.Find(u => u.Username == username && u.Password == password);
+            if (user != null)
+            {
+                CurrentUser = user;
+                return true;
+            }
+            return false;
+        }
+
+        public void Logout()
+        {
+            CurrentUser = null;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // För test: Lägg till användare manuellt
+        public void SeedUsers()
+        {
+            _users.Add(new User
+            {
+                Username = "admin",
+                Password = "1234",
+                DisplayName = "Admin",
+                Role = "Administrator"
+            });
+
+            _users.Add(new User
+            {
+                Username = "jonathan",
+                Password = "hemligt",
+                DisplayName = "Jonathan",
+                Role = "User"
+            });
+        }
+    }
+}
---
+++ CookMaster/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookMaster.Models
+{
+    public class User //skapar modellklass för att lagra data
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Role { get; set; }
+    }
+
+}
