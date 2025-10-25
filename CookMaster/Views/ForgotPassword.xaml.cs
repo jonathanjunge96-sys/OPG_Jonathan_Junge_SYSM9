a27e2311726af8b196493282dcc737346d1e20a5
Jonathan Junge <jonathan.junge96@gmail.com>
2025-10-25 16:31:26 +0200

    lagt till fönster

---
+++ CookMaster/Views/ForgotPassword.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CookMaster.Views
+{
+    /// <summary>
+    /// Interaction logic for ForgotPassword.xaml
+    /// </summary>
+    public partial class ForgotPassword : Window, INotifyPropertyChanged
+    {
+        public ForgotPassword()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            { 
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+
+            }
+        }
+
+        private void btnForgot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
